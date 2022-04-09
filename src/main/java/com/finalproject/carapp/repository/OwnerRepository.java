7bc0856c590db59a4224a8f7523850e81cefc6a5
Bora Yüret <borayuret@gmail.com>
2022-04-09 18:29:05 +0300

    İlk versiyon

---
+++ src/main/java/com/finalproject/carapp/repository/OwnerRepository.java
@@ -0,0 +1,8 @@
+package com.finalproject.carapp.repository;
+
+import com.finalproject.carapp.entity.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+
+}
